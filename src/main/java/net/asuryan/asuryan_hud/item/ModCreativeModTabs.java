6ff4ed011fe3dbb4440f9d88f464228a20c7d50a
Asuryan <asuryan2001@gmail.com>
2024-04-11 18:48:29 +0100

    recipes; fin vid4 (en fait)

--- src/main/java/net/asuryan/asuryan_hud/item/ModCreativeModTabs.java
+++ src/main/java/net/asuryan/asuryan_hud/item/ModCreativeModTabs.java
@@ -6,12 +6,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.awt.*;
 
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AsuryanHUD.MODID);
