9c24f26a6866ff854070654807940fbd7157188d
Asuryan <asuryan2001@gmail.com>
2024-04-12 18:29:14 +0100

    metal detector ; vid6 advanced items

--- src/main/java/net/asuryan/asuryan_hud/item/ModItems.java
+++ src/main/java/net/asuryan/asuryan_hud/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.asuryan.asuryan_hud.item;
 
 import net.asuryan.asuryan_hud.AsuryanHUD;
+import net.asuryan.asuryan_hud.item.custom.MetaldetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +13,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetaldetectorItem(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
